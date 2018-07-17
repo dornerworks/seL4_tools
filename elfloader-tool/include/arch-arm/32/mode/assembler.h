84295a921fdf6f496566dff1bbe98f32d7d1ac87
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2018-07-17 16:02:50 -0400

    Updated licensing in some code headers.
    Change-Id: Id59604245e6b99441f3edd4b4b065762b1bc9ccd

--- elfloader-tool/include/arch-arm/32/mode/assembler.h
+++ elfloader-tool/include/arch-arm/32/mode/assembler.h
@@ -1,11 +1,13 @@
 /*
  * Copyright 2014, General Dynamics C4 Systems
  *
+ * Copyright 2018, DornerWorks
+ *
  * This software may be distributed and modified according to the terms of
  * the GNU General Public License version 2. Note that NO WARRANTY is provided.
  * See "LICENSE_GPLv2.txt" for details.
  *
- * @TAG(GD_GPL)
+ * @TAG(DATA61_DORNERWORKS_GPL)
  */
 
 #pragma once
